9fceedb81d7692af415fcc1f45278d8c0e99f1bd
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2024-02-09 19:08:01 +0600

    Vite config done

--- src/component/firebase/firebase.config.js
+++ src/component/firebase/firebase.config.js
@@ -1,27 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_apiKey, 
-//   authDomain: import.meta.env.VITE_authDomain, 
-//   projectId: import.meta.env.VITE_projectId, 
-//   storageBucket: import.meta.env.VITE_storageBucket, 
-//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
-//   appId: import.meta.env.VITE_appId
-// };
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCHTLkLDxZUTXMAakia7jYUV6Lcd2dCCB4",
-  authDomain: "e-commers-webside.firebaseapp.com",
-  projectId: "e-commers-webside",
-  storageBucket: "e-commers-webside.appspot.com",
-  messagingSenderId: "969150754568",
-  appId: "1:969150754568:web:0155a1288c6ba4dde0c6bd"
+  apiKey: import.meta.env.VITE_apiKey, 
+  authDomain: import.meta.env.VITE_authDomain, 
+  projectId: import.meta.env.VITE_projectId, 
+  storageBucket: import.meta.env.VITE_storageBucket, 
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
